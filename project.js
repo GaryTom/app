b7c169b8c34c5bf2f68d62e9326f6afa3522848b
Gary Wang <qwetom324@gamil.com>
2020-05-31 22:44:35 +0800

    commit

--- project.js
+++ project.js
@@ -6,62 +6,16 @@ app.engine('ejs',engine);
 app.set('views','./views');
 app.set('view engine','ejs'); 
 
-//載入靜態檔案
-app.use(express.static('public'))
-
 
 //首頁路徑
 app.get('/',function(req,res){
-    //res.send(820);
-    //res.send('<html><head></head><body><img src="/image/5kZoWRZs.jpg"></body></html>');
     res.render('index');
-})
-//
-app.get('/form5',function(req,res){
-    //res.send(820);
-    //res.send('<html><head></head><body><img src="/image/5kZoWRZs.jpg"></body></html>');
-    res.render('form5');
-})
-
-app.get('/form6',function(req,res){
-    //res.send(820);
-    //res.send('<html><head></head><body><img src="/image/5kZoWRZs.jpg"></body></html>');
-    res.render('form6');
 })
 
 app.get('/todolist',function(req,res){
-    //res.send(820);
-    //res.send('<html><head></head><body><img src="/image/5kZoWRZs.jpg"></body></html>');
     res.render('firebase');
 })
 
-app.get('/picture',function(req,res){
-    //res.send(820);
-    res.send('<html><head></head><body><img src="/image/5kZoWRZs.jpg"></body></html>');
-
-})
-//常數的路徑
-app.get('/weather',function(req,res){
-    res.send('<html><head></head><body>出太陽</body></html>');
-})
-//變數的路徑
-app.get('/user/:name',function(req,res){
-    var Name = req.params.name;
-    if(Name!=='gary'){
-        res.send('<html><head></head><body>查無此人</body></html>');
-    }else{
-        res.send('<html><head></head><body>'+Name+'</body></html>');
-    }
-})
-//有參數的路徑
-app.get('/song/:name',function(req,res){
-    var Name = req.params.name;
-    var limit = req.query.limit;
-        res.send('<html><head></head><body>'+Name
-        +'聽'
-        +limit
-        +'首歌</body></html>');
-})
 
 //監聽
 var port = process.env.PORT || 3000;
